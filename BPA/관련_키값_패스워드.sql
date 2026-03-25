365a40b0669d72b6b2fa8690c2a48a12b94e964c
unknown <jwcho@cj.net>
2026-03-25 13:27:30 +0900

    Initial commit

---
+++ BPA/관련_키값_패스워드.sql
@@ -0,0 +1,12 @@
+제미나이 :
+AIzaSyAZz-BPvDpocLNMLpd97iBJDEGC5ZY-5QY
+
+    # [수정] 모델명을 models : gemini-2.5-flash 로 변경
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={api_key}"
+
+supabase : mZsD2edrLMnqyRpT
+
+Project URL :
+https://plhwwbjigsaypidcobrd.supabase.co
+Publishable Key : sb_publishable_yffSFbC1kkBw8vKMSXQzeA_qWoG3YIp
+Anon Key : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsaHd3YmppZ3NheXBpZGNvYnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMzE2NTIsImV4cCI6MjA4OTkwNzY1Mn0.AAtgL0e8GSisLDHb_caPM-HrcO3_3W041cq7XHo8BUA
