8dd66b05e8a1b559a3fdd16b247b27b18383e66d
unknown <jwcho@cj.net>
2026-03-27 15:39:20 +0900

    update

--- links.js
+++ links.js
@@ -7,7 +7,6 @@ var LINKS_DATA = {
     { "label": "DB: 점검 (ai)", "url": "./app/app_db_check_ai.html" },
     { "label": "DB: SQL 튜닝 (ai)", "url": "./app/app_db_sql_ai.html" },
     { "label": "Oracle: XPLAN", "url": "./app/app_oracle_xplan.html" },
-    { "label": "Oracle: 권한백업",  "url": "./app/app_oracle_grt.html" },
-    { "label": "자유게시판",  "url": "./app/app_bbs_free.html" }
+    { "label": "Oracle: 권한백업",  "url": "./app/app_oracle_grt.html" }
   ]
 };
