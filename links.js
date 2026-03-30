22b0bf22798531a006b0ab179b01faed9e08ec51
jwchob-lab <jwchob@gmail.com>
2026-03-30 04:29:55 +0000

    Deploying to gh-pages from @ jwchob-lab/bpa@2eae3c87a4b4fac484a5c4fef49c7d46afc94e05 🚀

--- links.js
+++ links.js
@@ -6,7 +6,8 @@ var LINKS_DATA = {
     { "label": "DB: 트리거",  "url": "./app/app_db_trigger.html" },
     { "label": "DB: 점검 (ai)", "url": "./app/app_db_check_ai.html" },
     { "label": "DB: SQL 튜닝 (ai)", "url": "./app/app_db_sql_ai.html" },
-    { "label": "Oracle: XPLAN", "url": "./app/app_oracle_xplan.html" },
+    { "label": "Oracle: Xplan", "url": "./app/app_oracle_xplan.html" },
+    { "label": "Oracle: Flashback", "url": "./app/app_oracle_flashback.html" },
     { "label": "Oracle: 권한백업",  "url": "./app/app_oracle_grt.html" }
   ]
 };
