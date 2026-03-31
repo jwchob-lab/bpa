e9268ddd108889aaa6821f890d953af1d41bb6f2
unknown <jwcho@cj.net>
2026-03-31 09:20:35 +0900

    Initial commit

---
+++ links.js
@@ -0,0 +1,13 @@
+var LINKS_DATA = {
+  "links": [
+    { "label": "디스크 사용량 예측",  "url": "./app/app_cm_disk.html" },
+    { "label": "디스크 사용량 예측 (ai)",  "url": "./app/app_cm_disk_ai.html" },
+    { "label": "보고서 작성 (ai)",  "url": "./app/app_cm_report_ai.html" },
+    { "label": "DB: 트리거",  "url": "./app/app_db_trigger.html" },
+    { "label": "DB: 점검 (ai)", "url": "./app/app_db_check_ai.html" },
+    { "label": "DB: SQL 튜닝 (ai)", "url": "./app/app_db_sql_ai.html" },
+    { "label": "Oracle: Xplan", "url": "./app/app_oracle_xplan.html" },
+    { "label": "Oracle: Flashback", "url": "./app/app_oracle_flashback.html" },
+    { "label": "Oracle: 권한백업",  "url": "./app/app_oracle_grt.html" }
+  ]
+};
